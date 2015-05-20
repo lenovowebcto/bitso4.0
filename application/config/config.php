6ecefb9b3cd7dae4b79cacda3d3a96f27fd5c935
ZHC <ZHC@ZHCdeMacBook-Pro.local>
2015-05-20 14:28:02 +0800

    edit config.php

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/bitso2/';
+$config['base_url']	= 'http://localhost/bitso4.0/';
 
 /*
 |--------------------------------------------------------------------------
