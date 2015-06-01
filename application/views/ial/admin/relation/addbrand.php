3e4af31ac46b85d001409e4f7d583d8d9a033e59
myselfofbin <libing87827@163.com>
2015-06-01 10:50:33 +0800

    brand table(dis) fields

--- application/views/ial/admin/relation/addbrand.php
+++ application/views/ial/admin/relation/addbrand.php
@@ -29,14 +29,24 @@
                       <form action="<?php echo site_url('Ial_admin/New_relation/toaddbrand') ?>" method="post">
                        <table>
 							<input name="bid"  value="<?php echo isset($bid)?$bid:0; ?>" type="hidden"/>
-							 <div  class="input-append date">
-							   <label>bname</label>
-							  <input type="text" name="bname"  value="<?php echo isset($brand['bname'])?$brand['bname']:'';?>"/>
+							
+							
+							 <div  class="input-append date" >
+							   <label>bname：</label>
+							  <input type="text" class="form-control" name="brand[bname]"  value="<?php echo isset($brand['bname'])?$brand['bname']:'';?>" style="width:200px"/>
 							  </div><br>
+							 <div class="input-append date">
+								<label>Dist:</label>
+	                             <select name="brand[dis]"  style="width:200px">
+							         <option class="form-control" <?php if(isset($brand['dis']) && $brand['dis']=='Think'){echo 'selected';}?> value="Think" >Think</option> 
+							         <option class="form-control" <?php if(isset($brand['dis']) && $brand['dis']=='Lenovo'){echo 'selected';}?> value="Lenovo" >Lenovo</option> 
+						         </select>
+							</div>
 							<div  class="input-append date">
 							   <label></label>
 							    <input type="SUBMIT" value="SUBMIT"/>
 							  </div>
+							  
 							  </table>
 						</form>
 					</div>
